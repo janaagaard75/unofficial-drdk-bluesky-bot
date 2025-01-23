6bc096b51c14fbf8ef15a4ef06ff0b5f923a9165
Jan Aagaard <jan@aagaard.net>
2025-01-23 21:11:57 +0100

    Basic script

---
+++ main.ts
@@ -0,0 +1,22 @@
+import { BskyAgent } from "@atproto/api";
+import * as dotenv from "dotenv";
+import * as process from "process";
+
+dotenv.config();
+
+const agent = new BskyAgent({
+  service: "https://bsky.social",
+});
+
+async function main() {
+  await agent.login({
+    identifier: process.env["BLUESKY_USERNAME"]!,
+    password: process.env["BLUESKY_PASSWORD"]!,
+  });
+  // await agent.post({
+  //   text: "🙂",
+  // });
+  console.log("Just posted!");
+}
+
+main();
