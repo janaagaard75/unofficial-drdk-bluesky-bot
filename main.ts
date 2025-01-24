ba569f6ad55fab1028b174f38ab21b200378a7eb
Jan Aagaard <jan@aagaard.net>
2025-01-24 22:53:53 +0100

    WIP

--- main.ts
+++ main.ts
@@ -1,32 +1,56 @@
 import { AtpAgent } from "@atproto/api";
-import Parser from "rss-parser";
 
 const agent = new AtpAgent({
   service: "https://bsky.social",
 });
 
-async function main() {
-  const parser = new Parser();
-  const newsFeed = await parser.parseURL(
-    "https://www.dr.dk/nyheder/service/feeds/senestenyt"
-  );
-
-  for (const item of newsFeed.items) {
-    console.log(item.title);
-  }
-
-  // await agent.login({
-  //   identifier: process.env["BLUESKY_USERNAME"]!,
-  //   password: process.env["BLUESKY_PASSWORD"]!,
-  // });
-
-  // try {
-  //   await agent.post({
-  //     text: "🙂",
-  //   });
-  // } catch (error) {
-  //   console.error(error);
-  // }
+interface External {
+  description: string;
+  thumb: string;
+  title: string;
+  uri: string;
 }
+
+async function main() {
+  // const parser = new Parser();
+  // const newsFeed = await parser.parseURL(
+  //   "https://www.dr.dk/nyheder/service/feeds/senestenyt"
+  // );
+
+  // for (const item of newsFeed.items) {
+  //   console.log(item.title);
+  // }
+
+  try {
+    await agent.login({
+      identifier: process.env["BLUESKY_USERNAME"]!,
+      password: process.env["BLUESKY_PASSWORD"]!,
+    });
+
+    const timeline = await agent.getTimeline({
+      limit: 50,
+    });
+
+    const feedViewPosts = timeline.data.feed;
+
+    const postedUris = feedViewPosts
+      .filter(
+        (feedViewPost) =>
+          feedViewPost.post.author.did === "did:plc:dlxnthdtaz7qdflht47dpst6"
+      )
+      .map((feedViewPost) => feedViewPost.post.embed)
+      .filter(isDefined)
+      .map((embed) => (embed.external as External).uri);
+
+    for (const uri of postedUris) {
+      console.log(uri);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const isDefined = <T>(item: T | null | undefined): item is T =>
+  item !== undefined && item !== null;
 
 main();
--- netlify-functions/post-new-news/post-new-news.mts
+++ netlify-functions/post-new-news/post-new-news.mts
@@ -1,10 +1,67 @@
+import { AtpAgent } from "@atproto/api";
 import type { Config } from "@netlify/functions";
+import Parser from "rss-parser";
+
+interface External {
+  description: string;
+  thumb: string;
+  title: string;
+  uri: string;
+}
 
 export default async (request: Request) => {
-  const { next_run } = await request.json();
+  try {
+    const requestJson = await request.json();
 
-  console.log("Received event! Next invocation at:", next_run);
+    const agent = new AtpAgent({
+      service: "https://bsky.social",
+    });
+
+    await agent.login({
+      identifier: process.env["BLUESKY_USERNAME"]!,
+      password: process.env["BLUESKY_PASSWORD"]!,
+    });
+
+    const timeline = await agent.getTimeline({
+      limit: 50,
+    });
+
+    const feedViewPosts = timeline.data.feed;
+
+    const postedUris = feedViewPosts
+      .filter(
+        (feedViewPost) =>
+          feedViewPost.post.author.did === "did:plc:dlxnthdtaz7qdflht47dpst6"
+      )
+      .map((feedViewPost) => feedViewPost.post.embed)
+      .filter(isDefined)
+      .map((embed) => (embed.external as External).uri);
+
+    const parser = new Parser();
+    const newsFeed = await parser.parseURL(
+      "https://www.dr.dk/nyheder/service/feeds/senestenyt"
+    );
+
+    const newItems = newsFeed.items
+      .filter((item) => item.link !== undefined)
+      .filter((item) => !postedUris.includes(item.link ?? ""));
+
+    for (const item of newItems) {
+      console.log(item.link);
+
+      await agent.post({
+        text: item.link,
+      });
+    }
+
+    console.log(`Received event. Next invocation at: ${requestJson.next_run}.`);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+const isDefined = <T,>(item: T | null | undefined): item is T =>
+  item !== undefined && item !== null;
 
 export const config: Config = {
   schedule: "@monthly",
