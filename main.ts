013df4ec032bd932a6a7f0d25b7dfb8941f897b5
Jan Aagaard <jan@aagaard.net>
2025-02-04 21:38:51 +0100

    Use the main function for testing

--- main.ts
+++ main.ts
@@ -1,151 +1,51 @@
-import { AtpAgent, Facet } from "@atproto/api";
-import Parser from "rss-parser";
-import { detectFacets, UnicodeString } from "./detectFacets";
-
-if (!Set.prototype.difference) {
-  Set.prototype.difference = function (
-    this: Set<any>,
-    otherSet: Set<any>
-  ): Set<any> {
-    const result = new Set(this);
-    for (const item of otherSet) {
-      result.delete(item);
-    }
-    return result;
-  };
-}
-
-if (!Set.prototype.intersection) {
-  Set.prototype.intersection = function (otherSet: Set<any>): Set<any> {
-    if (!(otherSet instanceof Set)) {
-      throw new TypeError("Argument must be a Set");
-    }
-
-    const intersectionSet = new Set<any>();
-    for (const item of this) {
-      if (otherSet.has(item)) {
-        intersectionSet.add(item);
-      }
-    }
-    return intersectionSet;
-  };
-}
-
-interface Record {
-  facets: Array<Facet>;
-}
-
-interface External {
-  description: string;
-  thumb: string;
-  title: string;
-  uri: string;
-}
+import { AtpAgent } from "@atproto/api";
+import { getEnvironmentVariableValue } from "./netlify-functions/post-new-links/getEnvironmentVariableValue";
 
 async function main() {
-  try {
-    const agent = new AtpAgent({
-      service: "https://bsky.social",
-    });
+  console.log("Starting...");
 
-    const postedUrls = await fetchPostedUrlsOnBluesky(agent);
-    const titlesAndUrlsFromFeed = await fetchTitlesAndUrlsFromRssFeed();
-    const urlsFromFeed = new Set(titlesAndUrlsFromFeed.map((item) => item.url));
-
-    // const alreadyPostedUrls = urlsFromFeed.intersection(postedUrls);
-
-    const newUrls = urlsFromFeed.difference(postedUrls);
-    const newTitlesAndUrls = titlesAndUrlsFromFeed.filter((titleAndUrl) =>
-      newUrls.has(titleAndUrl.url)
-    );
-
-    // console.log("postedUrls", postedUrls);
-    // console.log("urlsFromFeed", urlsFromFeed);
-    // console.log("alreadyPostedUrls", alreadyPostedUrls);
-    // console.log("newUrls", newUrls);
-
-    // const lastTitleAndUrl = newTitlesAndUrls[newTitlesAndUrls.length - 1];
-    // if (lastTitleAndUrl !== undefined) {
-    //   postUrl(agent, lastTitleAndUrl.title, lastTitleAndUrl.url);
-    // }
-
-    for (const titleAndUrl of newTitlesAndUrls) {
-      await postUrl(agent, titleAndUrl.title, titleAndUrl.url);
-    }
-
-    console.log(`Posted ${newUrls.size} new URLs.`);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const fetchTitlesAndUrlsFromRssFeed = async (): Promise<
-  Array<{
-    title: string;
-    url: string;
-  }>
-> => {
-  const parser = new Parser();
-  const newsFeed = await parser.parseURL(
-    "https://www.dr.dk/nyheder/service/feeds/senestenyt"
-  );
-
-  const titlesAndUrls = newsFeed.items.map((item) => {
-    return {
-      title: item.title ?? "",
-      url: item.link ?? "",
-    };
+  const agent = new AtpAgent({
+    service: "https://bsky.social",
   });
-
-  return titlesAndUrls;
-};
-
-const fetchPostedUrlsOnBluesky = async (
-  agent: AtpAgent
-): Promise<Set<string>> => {
+  const username = getEnvironmentVariableValue("BLUESKY_TEST_USERNAME");
+  const password = getEnvironmentVariableValue("BLUESKY_TEST_PASSWORD");
   await agent.login({
-    identifier: process.env["BLUESKY_USERNAME"]!,
-    password: process.env["BLUESKY_PASSWORD"]!,
+    identifier: username,
+    password: password,
   });
 
-  const feedSize = 20;
-  const timeline = await agent.getTimeline({
-    limit: 2 * feedSize,
-  });
+  console.log("Signed in.");
 
-  const feedViewPosts = timeline.data.feed;
-
-  const postedUrls = feedViewPosts
-    // Filter out posts that aren't from the bot. Don't know why this is necessary.
-    .filter(
-      (feedViewPost) =>
-        feedViewPost.post.author.did === "did:plc:dlxnthdtaz7qdflht47dpst6"
-    )
-    .map((feedViewPost) => feedViewPost.post.record as Record)
-    .filter(isDefined)
-    .flatMap((record) => record.facets)
-    .flatMap((facet) => facet.features)
-    .filter((feature) => feature.$type === "app.bsky.richtext.facet#link")
-    .map((link) => link.uri as string);
-
-  return new Set(postedUrls);
-};
-
-const postUrl = async (agent: AtpAgent, title: string, url: string) => {
-  const strippedUrl = url.replace(/https?:\/\//, "");
-  const text = `${title}\n\n${strippedUrl}`;
-  const facets = detectFacets(new UnicodeString(text));
   const post = {
-    facets: facets,
+    embed: {
+      $type: "app.bsky.embed.external",
+      external: {
+        description: "Hello World",
+        // thumb: {
+        //   $type: "blob",
+        //   mimeType: "image/png",
+        //   ref: {
+        //     $link:
+        //       "bafkreiash5eihfku2jg4skhyh5kes7j5d5fd6xxloaytdywcvb3r3zrzhu",
+        //   },
+        //   size: 23527,
+        // },
+        title: "External embed",
+        uri: "https://example.com",
+      },
+    },
     langs: ["da-DK"],
-    text: text,
+    text: "",
   };
 
   // console.dir(post, { depth: undefined });
   await agent.post(post);
-};
 
-const isDefined = <T>(item: T | null | undefined): item is T =>
-  item !== undefined && item !== null;
+  console.log("Done.");
+
+  return new Response("Posted Hello World to Bluesky.", {
+    status: 200,
+  });
+}
 
 main();
