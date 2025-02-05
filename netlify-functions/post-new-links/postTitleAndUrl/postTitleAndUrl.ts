98edd5935fa076faffb4774145e048882c29cc09
Jan Aagaard <jan@aagaard.net>
2025-02-05 22:33:04 +0100

    Switch back to posting as panels

--- netlify-functions/post-new-links/postTitleAndUrl/postTitleAndUrl.ts
+++ netlify-functions/post-new-links/postTitleAndUrl/postTitleAndUrl.ts
@@ -1,41 +1,22 @@
 import { AtpAgent } from "@atproto/api";
-import { detectFacets } from "./detectFacets";
 
 export const postTitleAndUrl = async (
   agent: AtpAgent,
   title: string,
   url: string
 ) => {
-  const strippedUrl = url.replace(/https?:\/\//, "");
-  const text = `${title}\n\n${strippedUrl}`;
-  const facets = detectFacets(text);
   const post = {
-    facets: facets,
+    embed: {
+      $type: "app.bsky.embed.external",
+      external: {
+        description: "",
+        title: title,
+        uri: url,
+      },
+    },
     langs: ["da-DK"],
-    text: text,
+    text: "",
   };
 
-  // console.dir(post, { depth: undefined });
   await agent.post(post);
 };
-
-// export const postTitleAndUrl = async (
-//   agent: AtpAgent,
-//   title: string,
-//   url: string
-// ) => {
-//   const post = {
-//     embed: {
-//       $type: "app.bsky.embed.external",
-//       external: {
-//         description: "",
-//         title: title,
-//         uri: url,
-//       },
-//     },
-//     langs: ["da-DK"],
-//     text: "",
-//   };
-
-//   await agent.post(post);
-// };
