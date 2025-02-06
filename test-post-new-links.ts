58871df0251b53d0024f52f231100a93a9d32f32
Jan Aagaard <jan@aagaard.net>
2025-02-06 21:51:07 +0100

    Test method

---
+++ test-post-new-links.ts
@@ -0,0 +1,46 @@
+import { AtpAgent } from "@atproto/api";
+import { fetchPostedUrlsOnBluesky } from "./netlify-functions/post-new-links/fetchPostedUrlsOnBluesky/fetchPostedUrlsOnBluesky";
+import { fetchTitlesAndUrlsFromRssFeed } from "./netlify-functions/post-new-links/fetchTitlesAndUrlsFromRssFeed";
+import { getEnvironmentVariableValue } from "./netlify-functions/post-new-links/getEnvironmentVariableValue";
+import { postTitleAndUrl } from "./netlify-functions/post-new-links/postTitleAndUrl/postTitleAndUrl";
+import { setDifference } from "./netlify-functions/post-new-links/setDifference";
+
+const main = async () => {
+  try {
+    const username = getEnvironmentVariableValue("BLUESKY_TEST_USERNAME");
+    const password = getEnvironmentVariableValue("BLUESKY_TEST_PASSWORD");
+    const agent = new AtpAgent({
+      service: "https://bsky.social",
+    });
+    await agent.login({
+      identifier: username,
+      password: password,
+    });
+    console.log(`Signed in to Bluesky as ${username}.`);
+
+    const postedUrls = await fetchPostedUrlsOnBluesky(agent);
+    console.log(`Fetched ${postedUrls.size} posted URLs.`);
+
+    const titlesAndUrlsFromFeed = await fetchTitlesAndUrlsFromRssFeed();
+    console.log(
+      `Fetched ${titlesAndUrlsFromFeed.length} titles and URLs from RSS feed.`
+    );
+
+    const urlsFromFeed = new Set(titlesAndUrlsFromFeed.map((item) => item.url));
+    const newUrls = setDifference(urlsFromFeed, postedUrls);
+
+    const newTitlesAndUrls = titlesAndUrlsFromFeed.filter((titleAndUrl) =>
+      newUrls.has(titleAndUrl.url)
+    );
+
+    for (const titleAndUrl of newTitlesAndUrls) {
+      await postTitleAndUrl(agent, titleAndUrl.title, titleAndUrl.url);
+    }
+
+    console.log(`Posted ${newTitlesAndUrls.length} new URLs.`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
