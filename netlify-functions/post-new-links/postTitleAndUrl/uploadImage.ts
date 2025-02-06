df2e1a82d80246ff6ec938cb030037fa0619ff15
Jan Aagaard <jan@aagaard.net>
2025-02-06 21:04:56 +0100

    Wrap in try-catch

--- netlify-functions/post-new-links/postTitleAndUrl/uploadImage.ts
+++ netlify-functions/post-new-links/postTitleAndUrl/uploadImage.ts
@@ -4,10 +4,15 @@ export const uploadImage = async (
   agent: AtpAgent,
   imageBuffer: ArrayBuffer | undefined
 ) => {
-  if (imageBuffer === undefined) {
+  try {
+    if (imageBuffer === undefined) {
+      return undefined;
+    }
+
+    const uploadedImage = await agent.uploadBlob(new Uint8Array(imageBuffer));
+    return uploadedImage?.data.blob;
+  } catch (error) {
+    console.error("Failed to upload image", error);
     return undefined;
   }
-
-  const uploadedImage = await agent.uploadBlob(new Uint8Array(imageBuffer));
-  return uploadedImage?.data.blob;
 };
