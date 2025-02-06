faa16949058c8ec78a89de0a7ce53b70ab512ea0
Jan Aagaard <jan@aagaard.net>
2025-02-06 21:03:37 +0100

    Wrap in a try-catch

--- netlify-functions/post-new-links/postTitleAndUrl/fetchDescriptionAndImage.ts
+++ netlify-functions/post-new-links/postTitleAndUrl/fetchDescriptionAndImage.ts
@@ -1,17 +1,22 @@
 import { extract } from "@extractus/article-extractor";
 
 export const fetchDescriptionAndImage = async (url: string) => {
-  const article = await extract(url);
+  try {
+    const article = await extract(url);
 
-  if (article?.image === undefined) {
+    if (article?.image === undefined) {
+      return undefined;
+    }
+
+    const downloadedImage = await fetch(article.image);
+    const imageBuffer = await downloadedImage.arrayBuffer();
+
+    return {
+      description: article.description,
+      image: imageBuffer,
+    };
+  } catch (error) {
+    console.error("Failed to fetch description and image", error);
     return undefined;
   }
-
-  const downloadedImage = await fetch(article.image);
-  const imageBuffer = await downloadedImage.arrayBuffer();
-
-  return {
-    description: article.description,
-    image: imageBuffer,
-  };
 };
