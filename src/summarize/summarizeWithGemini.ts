baac2a4a37fe6ff630b382e569940abdd2e737c5
Jan Aagaard <jan@aagaard.net>
2025-04-21 22:20:02 +0200

    Add summarizeWithGemini

---
+++ src/summarize/summarizeWithGemini.ts
@@ -0,0 +1,25 @@
+import { GoogleGenAI } from "@google/genai";
+import { getEnvironmentVariableValue } from "../getEnvironmentVariableValue";
+import { limitLength } from "./limitLength";
+
+export const summarizeWithGemini = async (
+  articleHtml: string,
+): Promise<string> => {
+  const ai = new GoogleGenAI({
+    apiKey: getEnvironmentVariableValue("GEMINI_API_KEY"),
+  });
+
+  const response = await ai.models.generateContent({
+    config: {
+      maxOutputTokens: 512,
+      responseMimeType: "text/plain",
+    },
+    contents: `Please summarize this text. Use a maximum of 300 characters.\n\n${articleHtml}`,
+    model: "gemini-2.5-flash-preview-04-17",
+  });
+  const summary = response.text ?? "";
+  console.log(`Gemini summary (${summary.length} characters): ${summary}`);
+
+  const limitedSummary = limitLength(summary);
+  return limitedSummary;
+};
