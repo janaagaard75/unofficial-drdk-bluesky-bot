aa33965795c7dacf63ad3ad72261c5c40bb69e99
Jan Aagaard <jan@aagaard.net>
2025-04-22 22:25:32 +0200

    Inline extractSummary

--- src/summarize/summarizeWithAzure.ts
+++ src/summarize/summarizeWithAzure.ts
@@ -1,13 +1,13 @@
 import {
   AnalyzeBatchAction,
   AzureKeyCredential,
+  PagedAnalyzeBatchResult,
   TextAnalysisClient,
 } from "@azure/ai-language-text";
 import { getEnvironmentVariableValue } from "../getEnvironmentVariableValue";
 import { HtmlString } from "../shared/HtmlString";
 import { PlainTextString } from "../shared/PlainTextString";
 import { extractArticleText } from "./extractArticleText";
-import { extractSummary } from "./extractSummary";
 
 export const summarizeWithAzure = async (
   articleHtml: HtmlString,
@@ -36,4 +36,36 @@ export const summarizeWithAzure = async (
   console.log(`Azure summary (${summary.length} characters): ${summary}`);
 
   return summary;
+};
+
+const extractSummary = async (
+  actionResults: PagedAnalyzeBatchResult,
+): Promise<PlainTextString> => {
+  const sentences: Array<PlainTextString> = [];
+
+  for await (const actionResult of actionResults) {
+    if (actionResult.kind !== "ExtractiveSummarization") {
+      throw new Error(
+        `Expected extractive summarization results but got: ${actionResult.kind}.`,
+      );
+    }
+
+    if (actionResult.error !== undefined) {
+      const { code, message } = actionResult.error;
+      throw new Error(`Unexpected error (${code}): ${message}`);
+    }
+
+    for (const result of actionResult.results) {
+      if (result.error !== undefined) {
+        const { code, message } = result.error;
+        throw new Error(`Unexpected error (${code}): ${message}`);
+      }
+
+      for (const sentence of result.sentences) {
+        sentences.push(sentence.text as PlainTextString);
+      }
+    }
+  }
+
+  return sentences.join(" ") as PlainTextString;
 };
