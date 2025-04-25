3a293b6698fa8fa618a7c9c04716370be15a37ed
Jan Aagaard <jan@aagaard.net>
2025-04-25 08:41:26 +0000

    Sleep to adhere to rate limits

--- src/summarize/summarizeWithGemini.ts
+++ src/summarize/summarizeWithGemini.ts
@@ -2,6 +2,7 @@ import { GoogleGenAI } from "@google/genai";
 import { getEnvironmentVariableValue } from "../getEnvironmentVariableValue";
 import { HtmlString } from "../shared/HtmlString";
 import { PlainTextString } from "../shared/PlainTextString";
+import { sleep } from "../shared/sleep";
 import { extractArticleText } from "./extractArticleText";
 
 export const summarizeWithGemini = async (
@@ -20,6 +21,10 @@ export const summarizeWithGemini = async (
     contents: `Please summarize this text. Use a maximum of 300 characters.\n\n${articleText}`,
     model: "gemini-2.5-flash-preview-04-17",
   });
+
+  // Add a delay to adhere to rate limits.
+  await sleep(400);
+
   const summary = (response.text ?? "") as PlainTextString;
   return summary;
 };
