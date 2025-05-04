338c636a5064e185c2e56fdf529aef18e9861918
Jan Aagaard <jan@aagaard.net>
2025-05-04 10:07:26 +0200

    Update and test extractImageUrl

--- src/extractImageUrl/extractImageUrl.ts
+++ src/extractImageUrl/extractImageUrl.ts
@@ -1,22 +1,22 @@
+import { JSDOM } from "jsdom";
 import { createUrlString } from "../shared/createUrlString";
 import { HtmlString } from "../shared/HtmlString";
 import { UrlString } from "../shared/UrlString";
 
 /** Extract the image URL from the content of the <meta name="og:image"> element in htmlDocument. */
 export const extractImageUrl = (
-  htmlDocument: HtmlString,
+  articleHtml: HtmlString,
 ): UrlString | undefined => {
-  const imageMatch = /<meta[^>]*property="og:image"[^>]*content="([^"]*)"/.exec(
-    htmlDocument,
+  const articleDocument = JSDOM.fragment(articleHtml);
+  const imageElement = articleDocument.querySelector(
+    "div[itemProp='image'] meta[itemProp='url']",
   );
+  const rawImageUrl = imageElement?.getAttribute("content");
 
-  if (imageMatch === null) {
+  if (rawImageUrl === null || rawImageUrl === undefined) {
     return undefined;
   }
 
-  if (imageMatch[1] === undefined) {
-    return undefined;
-  }
-
-  return createUrlString(imageMatch[1]);
+  const imageUrl = rawImageUrl.substring(0, rawImageUrl.indexOf("?"));
+  return createUrlString(imageUrl);
 };
