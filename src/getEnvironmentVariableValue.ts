4802af1f974caaae7c67f07259fb5252808eeb30
Jan Aagaard <jan@aagaard.net>
2026-02-12 20:38:46 +0100

    Only have plain text environment variables

--- src/getEnvironmentVariableValue.ts
+++ src/getEnvironmentVariableValue.ts
@@ -1,13 +1,12 @@
 import { PlainTextString } from "./shared/brandedTypes/PlainTextString";
-import { UrlString } from "./shared/brandedTypes/UrlString";
 
 export const getEnvironmentVariableValue = (
   variableName: string,
-): PlainTextString | UrlString => {
+): PlainTextString => {
   const value = process.env[variableName];
   if (value === undefined) {
     throw new Error(`${variableName} must be defined.`);
   }
 
-  return value as PlainTextString | UrlString;
+  return value as PlainTextString;
 };
