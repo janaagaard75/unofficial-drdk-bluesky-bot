66db4c352d1ad9eb6179efbe1ad5270df6bbf98c
Jan Aagaard <jan@aagaard.net>
2026-04-04 22:36:38 +0200

    Extract postHnStory

--- src/hackerNews/tests/testPostingHnStory.ts
+++ src/hackerNews/tests/testPostingHnStory.ts
@@ -1,48 +1,8 @@
-import { postToBluesky } from "../../postToBluesky/postToBluesky";
-import { brand } from "../../shared/brandedTypes/brand";
-import { PlainTextString } from "../../shared/brandedTypes/PlainTextString";
-import { UrlString } from "../../shared/brandedTypes/UrlString";
 import { testAgent } from "../../shared/testAgent";
-import { fetchHnStory } from "../fetchHnStory";
-import { fetchPageInfo } from "../fetchPageInfo";
 import { fetchRandomStoryId } from "../fetchRandomStoryId";
-import { summarize } from "../summarize";
+import { postHnStory } from "../postHnStory";
 
 const storyId = await fetchRandomStoryId();
 console.log(`Hacker News story ID: ${storyId}.`);
 
-const hnStory = await fetchHnStory(storyId);
-
-if (hnStory === undefined) {
-  throw new Error(
-    `Something went wrong fetching the story with ID ${storyId}.`,
-  );
-}
-
-console.log(`Hacker News title: ${hnStory.title}`);
-console.log(`Story URL: ${hnStory.url}`);
-
-const article = await fetchPageInfo(hnStory.url);
-
-console.log(`Title: ${article.title ?? "No title found."}`);
-console.log(`Description: ${article.description ?? "No description found."}`);
-console.log(`Image: ${article.imageUrl ?? "No image found."}`);
-console.log(
-  `Text: ${article.text === undefined ? "No text found." : article.text.substring(0, 600)}...`,
-);
-
-const summary = brand<PlainTextString>(
-  article.text === undefined
-    ? "No text found to summarize."
-    : await summarize(article.text, 300, "google/gemini-2.5-flash"),
-);
-console.log(`Summary: ${summary}`);
-
-await postToBluesky({
-  agent: testAgent,
-  linkDescription: summary,
-  linkImageUrl: article.imageUrl,
-  linkTitle: hnStory.title,
-  linkUrl: brand<UrlString>(`https://news.ycombinator.com/item?id=${storyId}`),
-  text: summary,
-});
+await postHnStory(testAgent, storyId);
