03e260041e161fea96e5124772a3e3f54a80139b
Jan Aagaard <jan@aagaard.net>
2026-04-04 23:10:54 +0200

    Add hnProductionAgent

--- src/hackerNews/postNewHnStories.ts
+++ src/hackerNews/postNewHnStories.ts
@@ -1,11 +1,11 @@
-import { drdkProductionAgent } from "../shared/drdkProductionAgent";
+import { hnProductionAgent } from "../shared/hnProductionAgent";
 import { getNewHnStoryIds } from "./getNewHnStoryIds";
 import { postHnStory } from "./postHnStory";
 
-const newStoryIds = await getNewHnStoryIds(drdkProductionAgent);
+const newStoryIds = await getNewHnStoryIds(hnProductionAgent);
 console.log("New story IDs: ", newStoryIds.join(", "));
 
 for (const storyId of newStoryIds) {
   console.log(`Posting story ID ${storyId} to Bluesky...`);
-  await postHnStory(drdkProductionAgent, storyId);
+  await postHnStory(hnProductionAgent, storyId);
 }
