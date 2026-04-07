4a0048898a5490a899154dc0e53eed58db9c2c03
Jan Aagaard <jan@aagaard.net>
2026-04-07 20:06:44 +0200

    Use setDifference

--- src/hackerNews/postNewHnStories.ts
+++ src/hackerNews/postNewHnStories.ts
@@ -3,7 +3,7 @@ import { hnProductionAgent } from "./hnProductionAgent";
 import { postHnStory } from "./postHnStory";
 
 const newStoryIds = await getNewHnStoryIds(hnProductionAgent);
-console.log("New story IDs: ", newStoryIds.join(", "));
+console.log("New story IDs: ", [...newStoryIds].join(", "));
 
 for (const storyId of newStoryIds) {
   console.log(`Posting story ID ${storyId} to Bluesky...`);
--- src/hackerNews/tests/testGetNewHnStoryIds.ts
+++ src/hackerNews/tests/testGetNewHnStoryIds.ts
@@ -3,4 +3,4 @@ import { getNewHnStoryIds } from "../getNewHnStoryIds";
 
 const newIds = await getNewHnStoryIds(testAgent);
 
-console.log("New story IDs: ", newIds.join(", "));
+console.log("New story IDs: ", [...newIds].join(", "));
