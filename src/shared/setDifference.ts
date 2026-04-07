b3b1a328987d147dbca3813f9a1808cf773393c0
Jan Aagaard <jan@aagaard.net>
2026-04-07 20:08:14 +0200

    Require that T is a call by value primitive

--- src/shared/setDifference.ts
+++ src/shared/setDifference.ts
@@ -1,4 +1,9 @@
-export const setDifference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+type Primitive = bigint | boolean | number | string | symbol | null | undefined;
+
+export const setDifference = <T extends Primitive>(
+  setA: Set<T>,
+  setB: Set<T>,
+): Set<T> => {
   const result = new Set(setA);
   for (const item of setB) {
     result.delete(item);
