c2fc973d6998ed881151fbc3802a4d05ba7a9c5d
Jan Aagaard <jaa@zenegy.com>
2026-04-08 23:21:12 +0200

    Pass stories to postHnStory

--- src/hackerNews/postNewHnStories.ts
+++ src/hackerNews/postNewHnStories.ts
@@ -1,3 +1,4 @@
+import { fetchHnStory } from "./fetchHnStory";
 import { getNewHnStoryIds } from "./getNewHnStoryIds";
 import { hnProductionAgent } from "./hnProductionAgent";
 import { postHnStory } from "./postHnStory";
@@ -6,6 +7,12 @@ const newStoryIds = await getNewHnStoryIds(hnProductionAgent);
 console.log("New story IDs: ", [...newStoryIds].join(", "));
 
 for (const storyId of newStoryIds) {
+  const hnStory = await fetchHnStory(storyId);
+  if (hnStory === undefined) {
+    console.log(`Story ID ${storyId} is invalid. Skipping...`);
+    continue;
+  }
+
   console.log(`Posting story ID ${storyId} to Bluesky...`);
-  await postHnStory(hnProductionAgent, storyId);
+  await postHnStory(hnProductionAgent, hnStory);
 }
--- src/hackerNews/tests/testPostingHnStory.ts
+++ src/hackerNews/tests/testPostingHnStory.ts
@@ -1,8 +1,16 @@
+import { exit } from "node:process";
 import { testAgent } from "../../shared/testAgent";
+import { fetchHnStory } from "../fetchHnStory";
 import { postHnStory } from "../postHnStory";
 import { fetchRandomStoryId } from "./fetchRandomStoryId";
 
 const storyId = await fetchRandomStoryId();
 console.log(`Hacker News story ID: ${storyId}.`);
 
-await postHnStory(testAgent, storyId);
+const hnStory = await fetchHnStory(storyId);
+if (hnStory === undefined) {
+  console.log(`Story ID ${storyId} is invalid.`);
+  exit(1);
+}
+
+await postHnStory(testAgent, hnStory);
